e8f6771ad20991d8e0cc91628acdee2d8ba55462
siddharthSingh2003 <sidsingh1679@gmail.com>
2024-03-28 18:15:10 +0530

    project 4 done

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -48,19 +48,47 @@ function validateGuess(guess){
 }
 
 function checkGuess(guess){
-    
+    if(guess === randomNumber){
+        displayMessage(`You gguessed it right`);
+        endGame();
+    }
+    else if( guess < randomNumber){
+        displayMessage(`Number is too LOw`)
+    }
+    else if( guess > randomNumber){
+        displayMessage(`Number is too high`)
+    }
 }
 function displayGuess(guess){
-
+    userInput.value = ''
+    guessSlot.innerHTML += `${guess}  `
+    numGuess++;remaining.innerHTML = `${ 11 - numGuess}`
 }
 function displayMessage(message){
-
+    lowOrHi.innerHTML = `<h2>${message}</h2>`
 }
 
 function newGame(){ 
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function(e){
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess =[]
+        numGuess =1
+        guessSlot.innerHTML = '';
+        remaining.innerHTML =`${11- numGuess}`;
+        userInput.removeAttribute('disabled');
+        startOver.removeChild(p);
+        playGame=true
+    })
 
 }
 
 function endGame(){
-
+    userInput.value = '';
+    userInput,setAttribute('disabled', '')
+    p.classList.add ('button')
+    p.innerHTML = '<h2 id="newGame>Start New Game</h2>';
+    startOver.appendChild(p);
+    playGame= false;
+    newGame();
 }
